51a4f4d465cc74d05ba28c871c1118471468414b
saikrishna4389 <saikrishnapathina@gmail.com>
2025-09-02 18:30:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ import ast
 import os
 from pathlib import Path
 
+# ✅ define BASE_DIR first
+BASE_DIR = Path(__file__).resolve().parent
+
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": os.getenv("FRONTEND_URL", "*")}})
 
@@ -167,5 +170,6 @@ def predict():
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
+
 
 
